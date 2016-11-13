1c015ed632f00307e9900472d1a5ebb8374f0a03
farhadno <nooralahzadeh@gmail.com>
2016-11-14 00:50:26 +0100

    layout of year component

--- app/components/YearForm.jsx
+++ app/components/YearForm.jsx
@@ -1,70 +1,61 @@
+
 var React=require('react');
 var {connect} = require('react-redux');
 var actions = require('actions');
 
 var FilteredMultiSelect = require('react-filtered-multiselect');
 
-const BOOTSTRAP_CLASSES = {
+const FOUNDATION_CLASSES = {
   filter: 'form-control',
   select: 'form-control',
-  button: 'btn btn btn-block btn-default',
+  button: 'button success hollow',
   buttonActive: 'btn btn btn-block btn-primary'
 }
 
-
 export var YearForm=React.createClass({
-
-  // handleDeselect(index) {
-  //   var selectedOptions = this.state.selectedOptions.slice()
-  //   selectedOptions.splice(index, 1)
-  //   this.setState(
-  //     {selectedOptions}
-  //   )
-  // },
-
-
-  handleSelectionChange(selectedOptions) {
-    debugger;
-    var {dispatch} = this.props;
-    selectedOptions.map((item)=> dispatch(actions.onSelectYear(item.id,item.name)));
-  },
 
   render:function(){
      //var {selectedOptions} = this.state;
       var {years,dispatch,selectedYears} = this.props;
       return(
       <div className="row">
-        <div className="col-md-5">
+        <div className="large-6 columns">
         <FilteredMultiSelect
           onChange={(selectedOptions)=>{
             dispatch(actions.onSelectYear(selectedOptions));
            }}
-           classNames={BOOTSTRAP_CLASSES}
+          classNames={FOUNDATION_CLASSES}
           options={years.years}
           selectedOptions={selectedYears}
           textProp="name"
           valueProp="id"
         />
        </div>
-     <div className="col-md-5">
+     <div className="large-6 columns">
         {selectedYears.length === 0 && <p>(nothing selected yet)</p>}
-        {selectedYears.length > 0 && <ul>
+        {selectedYears.length > 0 && <ol>
         {selectedYears.map((year, i) => <li key={year.id}>
             {`${year.name} `}
-            <button type="button" style={{marginLeft: 20}} className="btn btn-default" onClick={()=>{
+            <span style={{cursor: 'pointer'}} className="warning label" onClick={()=>{
                 dispatch(actions.onDeSelectYear(year.id));
             }}>
               &times;
-            </button>
-          </li>)}
-        </ul>}
+            </span>
+             </li>)}
+           </ol>}
+         {selectedYears.length > 0 && <button style={{marginLeft: 20}} className="button warning hollow" onClick={()=>{
+            dispatch(actions.onBackWard());
+          }}>
+          Clear Selection
+        </button>}
       </div>
+
     </div>
     );
     }
-  });
+});
 
-  export default connect(
+export default connect(
           (state) => {
             return state;
           }
