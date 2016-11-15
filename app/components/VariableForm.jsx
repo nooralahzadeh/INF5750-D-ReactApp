635c21ad14262b5a23d0b7ede557862832b8ec72
farhadno <nooralahzadeh@gmail.com>
2016-11-16 00:56:45 +0100

    variable form

--- app/components/VariableForm.jsx
+++ app/components/VariableForm.jsx
@@ -1,19 +1,46 @@
 var React=require('react');
 var ReactDom = require('react-dom');
-
+import Collapsible from 'react-collapsible';
 var VariableForm=React.createClass({
 
   render:function(){
 
       return(
-        <div>
-        <div>
-          <h3>Varibles  changes come here!</h3>
-        </div>
+        <div className="row">
+          <div className="large-6 columns">
+                <Collapsible trigger="Child health">
+                  <fieldset class="fieldset">
+                    <div>
+                           <input id="checkbox12" type="checkbox"/><label for="checkbox12">Checkbox 1</label>
+                    </div>
+                    <div>
+            <input id="checkbox22" type="checkbox"/><label for="checkbox22">Checkbox 2</label>
+            </div>
+            <div>
+          <input id="checkbox32" type="checkbox"/><label for="checkbox32">Checkbox 3</label>
+          </div>
+                  </fieldset>
+                </Collapsible>
+                  <Collapsible trigger="Immunisation">
+                    <fieldset class="fieldset">
+                      <div>
+                             <input id="checkbox12" type="checkbox"/><label for="checkbox12">Checkbox 1</label>
+                      </div>
+                      <div>
+              <input id="checkbox22" type="checkbox"/><label for="checkbox22">Checkbox 2</label>
+              </div>
+              <div>
+            <input id="checkbox32" type="checkbox"/><label for="checkbox32">Checkbox 3</label>
+            </div>
+                    </fieldset>
+                </Collapsible>
+          </div>
+            <div className="large-6 columns">
+            <p>another panel</p>
+            </div>
+            </div>
 
 
-        <button className="button expanded hollow">Import</button>
-    </div>
       );
     }
   });
